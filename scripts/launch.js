7422c7b51fd7aeefd39d4f4ab1ea1df17c2ee6d8
sanzaypant <sanzaypant@gmail.com>
2018-05-16 19:03:56 -0700

    Update launch.js

--- scripts/launch.js
+++ scripts/launch.js
@@ -1,27 +1,2 @@
-function lti_consumer_launch(id) {
-  var form = jQuery('form#launch-' + id);
 
-  if ( form.data('post') !== '' ) {
-    jQuery.post(
-      ajaxurl,
-      {action: 'lti_launch', post: form.data('post')}
-    );
-  }
 
-  form.submit();
-  dosomething();
-  
-}
-
-jQuery(document).ready(function () {
-  jQuery('form[data-auto-launch="yes"]').each(function () {
-    lti_consumer_launch(jQuery(this).data('id'));
-  });
-
-});
-
-function dosomething {
-  jQuery('form[data-auto-launch="yes"]').each(function () {
-    lti_consumer_launch(jQuery(this).data('id'));
-  });
-});
