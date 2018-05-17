cb64d1ae0f55878d4b98352c6d0fca53bb6d581f
sanzaypant <sanzaypant@gmail.com>
2018-05-17 05:11:32 -0700

    Update lti-consumer.php

--- lti-consumer.php
+++ lti-consumer.php
@@ -252,8 +252,8 @@ function sb_lti_launch_func($attrs) {
             $autolaunch = 'no';
         }
         $surl="https://zendo.datasciencedojo.com/hub/lti/launch";
-        $html .= "<form method=\"post\"  onsubmit=\"<?php sb_lti_launch_func($attrs) ?>\"
- action=\"$surl\" target=\"$target\" id=\"launch-$id\" data-id=\"$id\" data-post=\"$data[id]\" data-auto-launch=\"$autolaunch\">";
+        $html .= "<form method=\"post\"  onsubmit=$surl
+ action=\"<?php sb_lti_launch_func($attrs) ?>\" target=\"$target\" id=\"launch-$id\" data-id=\"$id\" data-post=\"$data[id]\" data-auto-launch=\"$autolaunch\">";
         foreach ( $data['parameters'] as $key => $value ) {
             $html .= "<input type=\"hidden\" name=\"$key\" value=\"$value\">";
         }
